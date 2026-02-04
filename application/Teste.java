847ab1c31e0ceeb1de5f474e609587aa8a6ae353
Givago Tavares <givago.mubarak1@gmail.com>
2026-02-04 04:16:30 -0300

    Tratamento de exceções

--- application/Teste.java
+++ application/Teste.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -13,68 +14,77 @@ import entities.Funcionario;
 
 public class Teste {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sft = new SimpleDateFormat("dd/MM/yyyy");
 		
 		List<Aluno> alunos = new ArrayList<Aluno>();
-		List<Funcionario> funcionarios = new ArrayList<Funcionario>(); 
+		List<Funcionario> funcionarios = new ArrayList<Funcionario>();
 		
-		int opcao;
-		do {
-			System.out.print("Digite a opção de cadastro: (aluno = 1/ funcionario = 2) ");
-			opcao = Integer.parseInt(sc.next());
-			
-			switch (opcao) {
-			case 1: 
-				System.out.print("Digite o nome do Aluno: ");
-				String nome = sc.nextLine();
-				sc.nextLine();
-				System.out.print("Digite a Data de Nascimento: ");
-				Date dataDeNascimento = sft.parse(sc.next());
-				System.out.print("Digite RG: ");
-				String rg = sc.nextLine();
-				sc.nextLine();
-				System.out.print("Digite Data da Matricula: ");
-				Date dataDaMatricula = sft.parse(sc.next());
-				System.out.print("Digite RA do Aluno: ");
-				String ra = sc.nextLine();
-				sc.nextLine();
+		try {
+		
+			int opcao;
+			do {
+				System.out.print("Digite a opção de cadastro: (aluno = 1/ funcionario = 2) ");
+				opcao = Integer.parseInt(sc.next());
 				
-				alunos.add(new Aluno(dataDeNascimento, nome, rg, dataDaMatricula, ra));
+				switch (opcao) {
+				case 1: 
+					System.out.print("Digite o nome do Aluno: ");
+					String nome = sc.nextLine();
+					sc.nextLine();
+					System.out.print("Digite a Data de Nascimento: ");
+					Date dataDeNascimento = sft.parse(sc.next());
+					System.out.print("Digite RG: ");
+					String rg = sc.nextLine();
+					sc.nextLine();
+					System.out.print("Digite Data da Matricula: ");
+					Date dataDaMatricula = sft.parse(sc.next());
+					System.out.print("Digite RA do Aluno: ");
+					String ra = sc.nextLine();
+					sc.nextLine();
+					
+					alunos.add(new Aluno(dataDeNascimento, nome, rg, dataDaMatricula, ra));
+					
+					break;
+				case 2:
+					System.out.print("Digite nome do Funcionario: ");
+					String nomeFuncionario = sc.nextLine();
+					sc.nextLine();
+					System.out.print("Digite data de Nascimento: ");
+					Date dataNascimentoFuncionario =sft.parse(sc.next());
+					System.out.print("Digite RG: ");
+					String rgFuncionario = sc.nextLine();
+					sc.nextLine();
+					System.out.print("Digite data de Admissão: ");
+					Date dataDeAdmissao = sft.parse(sc.next());
+					System.out.print("Digite função: ");
+					sc.nextLine();
+					String funcao = sc.nextLine();
+					System.out.print("Digite salario: ");
+					double salario = sc.nextDouble();
+					
+					funcionarios.add(new Funcionario(dataNascimentoFuncionario, nomeFuncionario, rgFuncionario, dataDeAdmissao, funcao, salario));
+					
+					break;
+				case 3:
+					System.out.println("Saindo...");
+					break;
 				
-				break;
-			case 2:
-				System.out.print("Digite nome do Funcionario: ");
-				String nomeFuncionario = sc.nextLine();
-				sc.nextLine();
-				System.out.print("Digite data de Nascimento: ");
-				Date dataNascimentoFuncionario =sft.parse(sc.next());
-				System.out.print("Digite RG: ");
-				String rgFuncionario = sc.nextLine();
-				sc.nextLine();
-				System.out.print("Digite data de Admissão: ");
-				Date dataDeAdmissao = sft.parse(sc.next());
-				System.out.print("Digite função: ");
-				sc.nextLine();
-				String funcao = sc.nextLine();
-				System.out.print("Digite salario: ");
-				double salario = sc.nextDouble();
+				default: System.out.println("Opção invalida. ");
+				}
 				
-				funcionarios.add(new Funcionario(dataNascimentoFuncionario, nomeFuncionario, rgFuncionario, dataDeAdmissao, funcao, salario));
-				
-				break;
-			case 3:
-				System.out.println("Saindo...");
-				break;
-			
-			default: System.out.println("Opção invalida. ");
-			}
-			
-		}while(opcao != 3);
-		sc.close();
+			}while(opcao != 3);
+			sc.close();
+		}
+		catch(ParseException e) {
+			System.out.println("Data invalida...");
+		}
+		catch (NumberFormatException e) {
+			System.out.println("Digite um numero valido...");
+		}
 	}
 
 }
